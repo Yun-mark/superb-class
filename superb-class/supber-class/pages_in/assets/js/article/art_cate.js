c86d76201ce09a6fec9dab3bdd423c120e3c3834
mark <1304101539@qq.com>
2021-07-25 09:54:24 +0800

    完成了周报类别的前端页面开发

--- superb-class/supber-class/pages_in/assets/js/article/art_cate.js
+++ superb-class/supber-class/pages_in/assets/js/article/art_cate.js
@@ -2,6 +2,8 @@ $(function(){
 
      var layer = layui.layer
 
+     var form =  layui.form
+
     initArtCateList()
     //获取文章分类的列表
   function initArtCateList(){
@@ -9,8 +11,7 @@ $(function(){
           method:'GET',
           url:'/my/article/cates',
           success: function(res){
-              console.log(res)
-            var htmlStr = template('tpl-table',res)
+          var htmlStr = template('tpl-table',res)
            $('tbody').html(htmlStr)  
         }
       })
@@ -56,7 +57,61 @@ $(function(){
       area:['500px','250px'],
       title:'修改文章分类',
       content: $('#dialog-edit').html()
+
+    })
+      var id = $(this).attr('data-id')
+      console.log(id)
+      // 发起请求获取对应分类的数据
+      $.ajax({
+        method: 'GET',
+        url: '/my/article/cates/' + id,
+        success: function(res) {
+          form.val('form-edit', res.data)
+        }
+  })
+
+  
+ 
+})
+
+// 通过代理的形式，为修改分类的表单绑定 submit 事件
+$('body').on('submit', '#form-edit', function(e) {
+  e.preventDefault()
+  $.ajax({
+    method: 'POST',
+    url: '/my/article/updatecate',
+    data: $(this).serialize(),
+    success: function(res) {
+      if (res.status !== 0) {
+        return layer.msg('更新分类数据失败！')
+      }
+      layer.msg('更新分类数据成功！')
+      layer.close(indexEdit)
+      initArtCateList()
+    }
+  })
+})
+
+// 通过代理的形式，为删除按钮绑定点击事件
+$('tbody').on('click', '.btn-delete', function() {
+  var id = $(this).attr('data-id')
+  // 提示用户是否要删除
+  layer.confirm('确认删除?', { icon: 3, title: '提示' }, function(index) {
+    $.ajax({
+      method: 'GET',
+      url: '/my/article/deletecate/' + id,
+      success: function(res) {
+        if (res.status !== 0) {
+          return layer.msg('删除分类失败！')
+        }
+        layer.msg('删除分类成功！')
+        layer.close(index)
+        initArtCateList()
+      }
     })
   })
+})
+
+   
 
 })
