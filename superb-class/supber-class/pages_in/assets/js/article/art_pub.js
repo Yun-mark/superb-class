dda6963ae3918cd616e8c452d8def9ccc05a037d
mark <1304101539@qq.com>
2021-07-25 15:39:39 +0800

    完成发布周报

--- superb-class/supber-class/pages_in/assets/js/article/art_pub.js
+++ superb-class/supber-class/pages_in/assets/js/article/art_pub.js
@@ -1,13 +1,12 @@
 $(function() {
-    var layer = layui.layer
-    var form =  layui.form
+  var layer = layui.layer
+  var form = layui.form
 
+  initCate()
+  // 初始化富文本编辑器
+  initEditor()
 
-    initCate()
-    //初始化富文本编辑器
-    initEditor()
-
-     // 定义加载文章分类的方法
+  // 定义加载文章分类的方法
   function initCate() {
     $.ajax({
       method: 'GET',
@@ -24,7 +23,42 @@ $(function() {
       }
     })
   }
-   // 定义文章的发布状态
+
+  // 1. 初始化图片裁剪器
+  var $image = $('#image')
+
+  // 2. 裁剪选项
+  var options = {
+    aspectRatio: 400 / 280,
+    preview: '.img-preview'
+  }
+
+  // 3. 初始化裁剪区域
+  $image.cropper(options)
+
+  // 为选择封面的按钮，绑定点击事件处理函数
+  $('#btnChooseImage').on('click', function() {
+    $('#coverFile').click()
+  })
+
+  // 监听 coverFile 的 change 事件，获取用户选择的文件列表
+  $('#coverFile').on('change', function(e) {
+    // 获取到文件的列表数组
+    var files = e.target.files
+    // 判断用户是否选择了文件
+    if (files.length === 0) {
+      return
+    }
+    // 根据文件，创建对应的 URL 地址
+    var newImgURL = URL.createObjectURL(files[0])
+    // 为裁剪区域重新设置图片
+    $image
+      .cropper('destroy') // 销毁旧的裁剪区域
+      .attr('src', newImgURL) // 重新设置图片路径
+      .cropper(options) // 重新初始化裁剪区域
+  })
+
+  // 定义文章的发布状态
   var art_state = '已发布'
 
   // 为存为草稿按钮，绑定点击事件处理函数
@@ -40,22 +74,24 @@ $(function() {
     var fd = new FormData($(this)[0])
     // 3. 将文章的发布状态，存到 fd 中
     fd.append('state', art_state)
-    // 将封面裁剪过后的图片，输出为一个文件对象
-    // $image
-    //   .cropper('getCroppedCanvas', {
-    //     // 创建一个 Canvas 画布
-    //     width: 400,
-    //     height: 280
-    //   })
-    //   .toBlob(function(blob) {
-    //     // 将 Canvas 画布上的内容，转化为文件对象
-    //     // 得到文件对象后，进行后续的操作
-    //    将文件对象，存储到 fd 中
-    //     fd.append('cover_img', blob)
-    //  4.发起 ajax 数据请求
-     publishArticle(fd)
-    //   })
+    // 4. 将封面裁剪过后的图片，输出为一个文件对象
+    $image
+      .cropper('getCroppedCanvas', {
+        // 创建一个 Canvas 画布
+        width: 400,
+        height: 280
+      })
+      .toBlob(function(blob) {
+        // 将 Canvas 画布上的内容，转化为文件对象
+        // 得到文件对象后，进行后续的操作
+        // 5. 将文件对象，存储到 fd 中
+        fd.append('cover_img', blob)
+        // 6. 发起 ajax 数据请求
+        publishArticle(fd)
+      })
   })
+
+  // 定义一个发布文章的方法
   function publishArticle(fd) {
     $.ajax({
       method: 'POST',
@@ -71,9 +107,8 @@ $(function() {
         }
         layer.msg('发布文章成功！')
         // 发布文章成功后，跳转到文章列表页面
-        location.href = './article/art_list.html'
+        location.href = './art_list.html'
       }
     })
-  } 
-
+  }
 })
