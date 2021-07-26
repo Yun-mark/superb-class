4271c18db2b16f6d9107ab1ca0e40831dfee57d3
wonder-z <2291487996@qq.com>
2021-07-26 17:06:52 +0800

    手机号密码修改部分样式基本完成，剩下验证和交互

--- superb-class/supber-class/pages_in/assets/js/user/user_safety.js
+++ superb-class/supber-class/pages_in/assets/js/user/user_safety.js
@@ -1,38 +1,103 @@
+
 $(function(){
   let form = layui.form;
-  let layer = layui.layer;
-  $('#changeIphone').on('click',function(){
-      layer.open({
-        type: 1,
-        area: ['600px', '360px'],
-        shadeClose: true, //点击遮罩关闭
-        content: `
-        <div class="small-box-phone">
-  <h2>修改手机号</h2>
-  <form class="layui-form" action="">
-    <div class="layui-form-item">
-      <label class="layui-form-label">输入框</label>
-      <div class="layui-input-block">
-        <input type="text" name="title" required  lay-verify="required" placeholder="请输入标题" autocomplete="off" class="layui-input">
-      </div>
-  </div>
-  <div class="layui-form-item">
-    <label class="layui-form-label">输入框</label>
-    <div class="layui-input-block">
-      <input type="text" name="title" required  lay-verify="required" placeholder="请输入标题" autocomplete="off" class="layui-input">
-    </div>
-</div>
-<div class="layui-form-item">
-  <label class="layui-form-label">输入框</label>
-  <div class="layui-input-block">
-    <input type="text" name="title" required  lay-verify="required" placeholder="请输入标题" autocomplete="off" class="layui-input">
-  </div>
-</div>
-  </form>
-</div>
-        `
-    });
-  }) 
-  // $('#changePassword').on('click', function(){})
- 
+  var show_num = [];
+  
+  // 修改手机号弹窗
+  $('#changeIphone').on('click', function(){
+    $('.phone-cover').show();
+    draw(show_num,document.getElementById('canvas'));
+    $('#canvas').prop('width','100');
+    $('#canvas').prop('height','43');
+    $(".words").on('click',function(){
+      draw(show_num,document.getElementById('canvas'));
+    })
+    $('.words').click();
+    $(".btn").on('click',function(){
+      var val = $(".image-code").val().toLowerCase();
+      var num = show_num.join("");
+      if(val==''){
+          alert('请输入验证码！');
+      }else if(val == num){
+          alert('提交成功！');
+          $(".image-code").val('');
+          draw(show_num,document.getElementById('canvas'));
+
+      }else{
+          alert('验证码错误！请重新输入！');
+          $(".image-code").val('');
+          draw(show_num,document.getElementById('canvas'));
+      }
+  })
+  })
+  $('.phone-close').click(function() {
+      $(".phone-cover").hide();
+  });
+
+
+
+  // 修改密码弹窗
+  $('#changePassword').on('click', function(){
+    $('.password-cover').show();
+    draw(show_num,document.getElementById('canvasPassword'));
+
+    $('#canvasPassword').prop('width','100');
+    $('#canvasPassword').prop('height','43');
+    $(".words").on('click',function(){
+      draw(show_num,document.getElementById('canvasPassword'));
+  })
+    $('.words').click();
+    $(".btnPass").on('click',function(){
+      var val = $(".image-code-password").val().toLowerCase();
+      var num = show_num.join("");
+      if(val==''){
+          alert('请输入验证码！');
+      }else if(val == num){
+          alert('提交成功！');
+          $(".image-code-password").val('');
+          draw(show_num,document.getElementById('canvasPassword'));
+
+      }else{
+          alert('验证码错误！请重新输入！');
+          $(".image-code-password").val('');
+          draw(show_num,document.getElementById('canvasPassword'));
+      }
+  })
+})
+  $('.password-close').click(function() {
+      $(".password-cover").hide();
+  });
+
+  //查看密码
+  $('.eye').each(function(index, domEle){
+    $(domEle).on('click', function(){
+      let i = index;
+      let e = $(domEle)
+      $('.eye-password').each(function(index, domEle){
+        if(i === index && $(domEle).prop("type") === "password"){
+          e.prop("src","../assets/images/eye-open.png");
+          $(domEle).prop("type", "text");
+        }
+        else if(i === index && $(domEle).prop("type") === "text"){
+          e.prop("src","../assets/images/eye-close.png");
+          $(domEle).prop("type", "password");
+        }
+      })
+    })
+  })
+  
+
+
+
+  
+    
+  //获取手机号
+  // $.ajax({
+    
+  // })
+  
+
+
+
+
 })
