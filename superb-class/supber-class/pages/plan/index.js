9e0c80ce3b4d0d397d28a4747129ea9b3a041bcf
Oscar <2547239232@qq.com>
2021-07-27 17:29:22 +0800

    优化成长计划

--- superb-class/supber-class/pages/plan/index.js
+++ superb-class/supber-class/pages/plan/index.js
@@ -21,42 +21,13 @@ class Tab{
     toggleTab(){
         that.clearClass();
         that.sections[this.index].className = "conactive";
-        if(this.index == 0)
-        {
-            this.className = 'liAfter li21';
-        }
-        else if(this.index == 1)
-        {
-            this.className = 'liAfter li22';
-        }
-        else if(this.index == 2)
-        {
-            that.lis[1].className = 'liBefore li112';
-            this.className = 'liAfter li23';
-        }
-        else if(this.index == 3)
-        {
-            that.lis[1].className = 'liBefore li112';
-            that.lis[2].className = 'liBefore li113';
-            this.className = 'liAfter li24';
-        }
-        else if(this.index == 4)
-        {
-            that.lis[1].className = 'liBefore li112';
-            that.lis[2].className = 'liBefore li113';
-            that.lis[3].className = 'liBefore li114';
-            this.className = 'liAfter li25';
-        }
+        that.lis[this.index].className = 'liAfter';
     }
     clearClass(){
         for(var i=0; i<this.lis.length; i++){
             this.sections[i].className = "delcon";
+            this.lis[i].className = 'liBefore';
         }
-        this.lis[0].className = 'liBefore li11';
-        this.lis[1].className = 'liBefore li12';
-        this.lis[2].className = 'liBefore li13';
-        this.lis[3].className = 'liBefore li14';
-        this.lis[4].className = 'liBefore li15';
     }
     
 }
